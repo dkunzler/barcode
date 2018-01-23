06f4b9d39fcb030cbac95dd8c9505e683bb5cb8c
David Kunzler <dk@devland.de>
2018-01-23 23:18:29 +0100

    keep result when rotating screen

--- app/src/main/java/de/devland/scanner/ResultFragment.kt
+++ app/src/main/java/de/devland/scanner/ResultFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.google.android.gms.vision.barcode.Barcode
 import com.squareup.otto.Subscribe
 import de.devland.scanner.event.BarcodeEvent
 import kotterknife.bindView
@@ -19,6 +20,8 @@ class ResultFragment : Fragment() {
 
     private val toolbar: Toolbar by bindView(R.id.toolbar)
     private val resultText: TextView by bindView(R.id.resultText)
+
+    private var currentBarcode: Barcode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,14 +35,15 @@ class ResultFragment : Fragment() {
 
     @Subscribe
     fun onBarcodeEvent(barcodeEvent: BarcodeEvent) {
+        currentBarcode = barcodeEvent.barcode
         // try to parse a json object
-        try {
-            val json = JSONObject(barcodeEvent.barcode.rawValue)
-            resultText.text = json.toString(2)
-        } catch (e: JSONException) {
-            // parsing failed, use raw text and Linkify for result
-            resultText.text = barcodeEvent.barcode.rawValue
-            Linkify.addLinks(resultText, Linkify.ALL)
+        processBarcode(barcodeEvent.barcode)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (currentBarcode != null) {
+            outState.putParcelable("result", currentBarcode)
         }
     }
 
@@ -47,9 +51,26 @@ class ResultFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         toolbar.setTitle(R.string.app_name)
         resultText.typeface = Typeface.createFromAsset(context.assets, "fonts/RobotoSlab-Light.ttf")
+        if (savedInstanceState != null && savedInstanceState.containsKey("result")) {
+            val barcode: Barcode = savedInstanceState.getParcelable("result")
+            currentBarcode = barcode
+            processBarcode(barcode)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_result, container, false)
+    }
+
+
+    private fun ResultFragment.processBarcode(barcode: Barcode) {
+        try {
+            val json = JSONObject(barcode.rawValue)
+            resultText.text = json.toString(2)
+        } catch (e: JSONException) {
+            // parsing failed, use raw text and Linkify for result
+            resultText.text = barcode.rawValue
+            Linkify.addLinks(resultText, Linkify.ALL)
+        }
     }
 }
