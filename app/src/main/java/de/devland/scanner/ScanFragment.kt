2595f8385fa0ee33166b9b24fcb1f839f1a0f4eb
David Kunzler <dk@devland.de>
2021-11-11 23:04:58 +0100

    change scanner engine to zbar

--- app/src/main/java/de/devland/scanner/ScanFragment.kt
+++ app/src/main/java/de/devland/scanner/ScanFragment.kt
@@ -14,20 +14,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.samples.vision.barcodereader.BarcodeGraphic
 import com.google.android.gms.samples.vision.barcodereader.BarcodeTrackerFactory
-import com.google.android.gms.samples.vision.barcodereader.ui.camera.CameraSourcePreview
 import com.google.android.gms.samples.vision.barcodereader.ui.camera.GraphicOverlay
-import com.google.android.gms.vision.CameraSource
-import com.google.android.gms.vision.MultiProcessor
-import com.google.android.gms.vision.barcode.Barcode
-import com.google.android.gms.vision.barcode.BarcodeDetector
 import com.squareup.otto.Subscribe
 import de.devland.scanner.event.FragmentSelectionEvent
 import de.devland.scanner.event.FragmentType
+import kotlinx.android.synthetic.main.fragment_scan.*
 import kotterknife.bindView
+import me.dm7.barcodescanner.zbar.ZBarScannerView
 import java.io.IOException
 
 
@@ -39,10 +34,7 @@ class ScanFragment : Fragment() {
         const val RC_HANDLE_GMS = 9001
     }
 
-    private var cameraSource: CameraSource? = null
-    private val scanPreview: CameraSourcePreview by bindView(R.id.preview)
-    private val graphicOverlay: GraphicOverlay<BarcodeGraphic> by bindView(R.id.graphicOverlay)
-    private val topLayout: View by bindView(R.id.topLayout)
+    private val scanPreview: ZBarScannerView by bindView(R.id.preview)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,7 +73,7 @@ class ScanFragment : Fragment() {
         }
 
         topLayout.setOnClickListener(listener)
-        Snackbar.make(graphicOverlay, R.string.permission_camera_rationale,
+        Snackbar.make(scanPreview, R.string.permission_camera_rationale,
                 Snackbar.LENGTH_INDEFINITE)
                 .setAction(android.R.string.ok, listener)
                 .show()
@@ -97,53 +89,6 @@ class ScanFragment : Fragment() {
      */
     @SuppressLint("InlinedApi")
     private fun createCameraSource() {
-        val context = activity.applicationContext
-
-        // A barcode detector is created to track barcodes.  An associated multi-processor instance
-        // is set to receive the barcode detection results, track the barcodes, and maintain
-        // graphics for each barcode on screen.  The factory is used by the multi-processor to
-        // create a separate tracker instance for each barcode.
-        val barcodeDetector = BarcodeDetector.Builder(context).build()
-        val barcodeFactory = BarcodeTrackerFactory(graphicOverlay, activity)
-        barcodeDetector.setProcessor(
-                MultiProcessor.Builder<Barcode>(barcodeFactory).build())
-
-        if (!barcodeDetector.isOperational) {
-            // Note: The first time that an app using the barcode or face API is installed on a
-            // device, GMS will download a native libraries to the device in order to do detection.
-            // Usually this completes before the app is run for the first time.  But if that
-            // download has not yet completed, then the above call will not detect any barcodes
-            // and/or faces.
-            //
-            // isOperational() can be used to check if the required native libraries are currently
-            // available.  The detectors will automatically become operational once the library
-            // downloads complete on device.
-            Log.w(TAG, "Detector dependencies are not yet available.")
-
-            // Check for low storage.  If there is low storage, the native library will not be
-            // downloaded, so detection will not become operational.
-            @Suppress("DEPRECATION")
-            val lowStorageFilter = IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW)
-            val hasLowStorage = activity.registerReceiver(null, lowStorageFilter) != null
-
-            if (hasLowStorage) {
-                Toast.makeText(activity, R.string.low_storage_error, Toast.LENGTH_LONG).show()
-                Log.w(TAG, getString(R.string.low_storage_error))
-            }
-        }
-
-        // Creates and starts the camera.  Note that this uses a higher resolution in comparison
-        // to other detection examples to enable the barcode detector to detect small barcodes
-        // at long distances.
-        var builder: CameraSource.Builder = CameraSource.Builder(context, barcodeDetector)
-                .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedPreviewSize(1600, 1024)
-                .setRequestedFps(15.0f)
-
-        // make sure that auto focus is an available option
-        builder = builder.setAutoFocusEnabled(true)
-
-        cameraSource = builder.build()
     }
 
     /**
